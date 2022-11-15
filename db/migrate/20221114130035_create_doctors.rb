c0f1e8ca6a086b9ab473d7ec60bd00db3a3cd847
Ismail Aidar <ismailaidar@gmail.com>
2022-11-15 08:04:22 -0500

    change syntax npi index

--- db/migrate/20221114130035_create_doctors.rb
+++ db/migrate/20221114130035_create_doctors.rb
@@ -1,10 +1,9 @@
 class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors, id: false do |t|
-      t.string :npi, null: false
+      t.string :npi, null: false, index: { unique: true }
       t.references :person, null: false, foreign_key: true, primary_key: true
       t.check_constraint "npi::text ~ '^[0-9]{10}$'::text", name: "npi_check"
-      t.index :npi, name: 'check_npi_unique', unique: true
 
       t.timestamps
     end
