218be7a1c5db22f984cb88592a945db8685fb2cc
Ismail Aidar <ismailaidar@gmail.com>
2022-11-15 11:13:31 -0500

    add controller doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'doctors/index'
+  get 'doctors/show'
+  get 'doctors/new'
+  get 'doctors/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
