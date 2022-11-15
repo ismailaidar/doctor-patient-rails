db1197119099d58e628d1a36da7eda54c08c9084
Ismail Aidar <ismailaidar@gmail.com>
2022-11-15 11:12:09 -0500

    add models person and doctor

---
+++ app/models/doctor.rb
@@ -0,0 +1,8 @@
+class Doctor < ApplicationRecord
+    belongs_to :person
+    accepts_nested_attributes_for :person, allow_destroy: true
+      
+    validates :npi, presence: true, allow_blank: false, length: { is: 10}, format: { with: /\A[0-9]+\z/,
+        message: "only allows numbers" }, uniqueness: true
+end
+  
