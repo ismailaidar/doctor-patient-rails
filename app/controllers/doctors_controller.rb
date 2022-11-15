2c530ee8b6864799e9435346e9e888fd1ce15c03
Ismail Aidar <ismailaidar@gmail.com>
2022-11-15 11:48:42 -0500

    add actions index create

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,13 +1,37 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: %i[ show edit update  ]
   def index
+    @doctors = Doctor.order("person_id DESC")
   end
 
   def show
   end
 
   def new
+    @doctor = Doctor.new
+    @doctor.build_person 
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    @doctor.build_person doctor_params[:person_attributes]
+    if @doctor.commit
+      redirect_to doctor_url(@doctor), notice: "Doctor was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  
   end
 
   def edit
+  end
+
+  private
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:npi, person_attributes: [:first_name, :last_name] )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'doctors/index'
-  get 'doctors/show'
-  get 'doctors/new'
-  get 'doctors/edit'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :doctors
 end
