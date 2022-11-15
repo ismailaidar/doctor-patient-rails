02d3de6fc569dd69bafae8c21b8be6eb47f19a2c
Ismail Aidar <ismailaidar@gmail.com>
2022-11-15 11:50:35 -0500

    add function commit

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def commit
+    save(validate: false)
+  rescue ActiveRecord::ActiveRecordError 
+    valid?
+    # if self.class == Doctor
+  end
 end
