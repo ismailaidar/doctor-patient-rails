68e55d5123c9f9f2c8c236d77cacd1ce75204e72
Ismail Aidar <ismailaidar@gmail.com>
2022-11-15 21:08:42 -0500

    2-space indents in HTML

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,14 +1,12 @@
 class Doctor < ApplicationRecord
-    belongs_to :person
-    accepts_nested_attributes_for :person, allow_destroy: true
-      
-    validates :npi, presence: true, allow_blank: false, length: { is: 10}, format: { with: /\A[0-9]+\z/,
-        message: "only allows numbers" }, uniqueness: true
+	belongs_to :person
+	accepts_nested_attributes_for :person, allow_destroy: true
 
-    
-    def full_name
-        "#{self.person.first_name} #{self.person.last_name}"
-    end
+	validates :npi, presence: true, allow_blank: false, length: { is: 10}, format: { with: /\A[0-9]+\z/,
+			message: "only allows numbers" }, uniqueness: true
 
+	def full_name
+			"#{self.person.first_name} #{self.person.last_name}"
+	end
 end
   
