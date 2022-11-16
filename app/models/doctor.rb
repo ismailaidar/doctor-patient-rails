643448561099483719f730e4db88cd047bc52bfd
Ismail Aidar <ismailaidar@gmail.com>
2022-11-16 09:35:49 -0500

    Defining full_name on Person

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,9 +4,13 @@ class Doctor < ApplicationRecord
 
 	validates :npi, presence: true, allow_blank: false, length: { is: 10}, format: { with: /\A[0-9]+\z/,
 			message: "only allows numbers" }, uniqueness: true
+	validate :first_name_last_name_check_blank
 
-	def full_name
-			"#{self.person.first_name} #{self.person.last_name}"
+	private
+	def first_name_last_name_check_blank
+		errors.add( :base, "first name can't be blank" ) if person.first_name == ''
+		errors.add( :base, "last name can't be blank" ) if person.last_name == ''
 	end
+	
 end
   
