be98a1271d0665cd6b56db3ea0d0f181b532a7f6
Ismail Aidar <ismailaidar@gmail.com>
2022-11-16 16:17:37 -0500

    fix validation doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,15 +2,9 @@ class Doctor < ApplicationRecord
 	belongs_to :person
 	accepts_nested_attributes_for :person, allow_destroy: true
 
-	validates :npi, presence: true, allow_blank: false, length: { is: 10}, format: { with: /\A[0-9]+\z/,
+	validates :npi, presence: true, length: { is: 10}, format: { with: /\A[0-9]+\z/,
 			message: "only allows numbers" }, uniqueness: true
-	validate :first_name_last_name_check_blank
-
-	private
-	def first_name_last_name_check_blank
-		errors.add( :base, "first name can't be blank" ) if person.first_name == ''
-		errors.add( :base, "last name can't be blank" ) if person.last_name == ''
-	end
+	
 	
 end
   
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,5 +3,6 @@ class Person < ApplicationRecord
 	def full_name
 			"#{first_name} #{last_name}"
 	end
+	validates :first_name, :last_name,  presence: true
 
 end
