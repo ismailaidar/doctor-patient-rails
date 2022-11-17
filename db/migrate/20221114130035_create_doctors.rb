c49a4f16b23c6e3ca4f344eade4d8df2fe4bd799
Ismail Aidar <ismailaidar@gmail.com>
2022-11-17 11:53:20 -0500

    add status to doctor migration

--- db/migrate/20221114130035_create_doctors.rb
+++ db/migrate/20221114130035_create_doctors.rb
@@ -2,6 +2,7 @@ class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors, id: false do |t|
       t.string :npi, null: false, index: { unique: true }
+      t.integer :status, default: 0
       t.references :person, null: false, foreign_key: true, primary_key: true
       t.check_constraint "npi::text ~ '^[0-9]{10}$'::text", name: "npi_check"
 
