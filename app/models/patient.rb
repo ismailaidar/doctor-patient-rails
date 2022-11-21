79ca24b848a7a5c8b66043076a758cb54f820df1
Ismail Aidar <ismailaidar@gmail.com>
2022-11-21 14:09:02 -0500

    add validation to upi

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,7 @@
 class Patient < ApplicationRecord
-	belongs_to :person
-	belongs_to :doctor,  optional: true
-	validates :upi, presence: true
+  belongs_to :person
+  belongs_to :doctor
+  validates :upi, presence: true, length: { is: 18 },
+                  format: { with: /\A[A-Za-z0-9]+\z/, message: 'only allows Alphanumeric' },
+                  uniqueness: true
 end
