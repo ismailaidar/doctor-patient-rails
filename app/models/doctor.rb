bcd00b0439ccc20fc6aff1dad7b0af18c2b0bdbc
Ismail Aidar <ismailaidar@gmail.com>
2022-11-21 14:05:39 -0500

    change leave to leaved and retire to retired

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ApplicationRecord
   belongs_to :person
   has_many :appointments
   enum :status, {
-    active: 'active', leave: 'leave', retire: 'retire'
+    active: 'active', leaved: 'leaved', retired: 'retired'
   }, default: 'active', prefix: true
   validates :person, uniqueness: true
   validates :npi, presence: true, length: { is: 10 }, format: { with: /\A[0-9]+\z/, message: 'only allows numbers' },
