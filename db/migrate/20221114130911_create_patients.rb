bdfe23dfd141fcfa0effa68cc3a5a09e1311811d
Ismail Aidar <ismailaidar@gmail.com>
2022-11-22 14:20:34 -0500

    add link patients to navbar / fix spaces on patient migration

--- db/migrate/20221114130911_create_patients.rb
+++ db/migrate/20221114130911_create_patients.rb
@@ -4,12 +4,11 @@ class CreatePatients < ActiveRecord::Migration[7.0]
       t.string :upi, null: false
       t.references :person, null: false, foreign_key: true, primary_key: true
       t.references :doctor, null: false, foreign_key: { primary_key: :person_id }
-      t.check_constraint "upi::text ~ '^[a-z0-9]{18}$'::text", name: "upi_check"
+      t.check_constraint "upi::text ~ '^[a-z0-9]{18}$'::text", name: 'upi_check'
       t.index :upi, name: 'check_upi_unique', unique: true
-      t.check_constraint "doctor_id <> person_id", name: "check_if_dr_and_person_are_different"
+      t.check_constraint 'doctor_id <> person_id', name: 'check_if_dr_and_person_are_different'
 
       t.timestamps
     end
-
   end
 end
