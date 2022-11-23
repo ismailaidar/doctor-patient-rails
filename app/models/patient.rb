957ba286286d3bd6842c4376d121429e3762ba9f
Ismail Aidar <ismailaidar@gmail.com>
2022-11-23 08:51:58 -0500

    add validation check_if_dr_and_pr_are_diff for model's patient

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,17 @@
 class Patient < ApplicationRecord
   belongs_to :person
   belongs_to :doctor
+
+  # validates :doctor, presence: true
+  # validates :person, presence: true
   validates :upi, presence: true, length: { is: 18 },
                   format: { with: /\A[A-Za-z0-9]+\z/, message: 'only allows Alphanumeric' },
                   uniqueness: true
+  validate :check_if_dr_and_pr_are_diff
+
+  def check_if_dr_and_pr_are_diff
+    return unless doctor
+
+    errors.add(:base, 'doctor and patient are the same person') if person == doctor.person
+  end
 end
