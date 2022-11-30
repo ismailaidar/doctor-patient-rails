160c17e1c987e1f876be4c92de4e15673880e188
Ismail Aidar <ismailaidar@gmail.com>
2022-11-30 08:56:16 -0500

    upi_check lowercase letters + digits

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,6 @@ class Patient < ApplicationRecord
   belongs_to :person
   belongs_to :doctor
   validates :upi, presence: true, length: { is: 18 },
-                  format: { with: /\A[A-Za-z0-9]+\z/, message: 'only allows Alphanumeric' },
+                  format: { with: /\A[a-z0-9]+\z/, message: 'only allows Alphanumeric' },
                   uniqueness: true
 end
