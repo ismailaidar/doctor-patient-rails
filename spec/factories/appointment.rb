e40d1adb43795fb9b825070e84fa1b1e0dd6b343
Ismail Aidar <ismailaidar@gmail.com>
2022-12-01 09:30:49 -0500

    Foreign keys should be implemented through FactoryBot associations (Appointment)

--- spec/factories/appointment.rb
+++ spec/factories/appointment.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :appointment do
-    doctor_id { '1234567891' }
-    patient_id { -1 }
-    timerange { '[2022-11-11T09:32, 2022-11-11T10:32)' }
+    doctor
+    patient
+    timerange do
+      start = Faker::Time.between(from: 1.year.ago, to: Time.now)
+      stop = Faker::Time.between(from: start, to: start + 3.hours)
+      start...stop
+    end
   end
 end
