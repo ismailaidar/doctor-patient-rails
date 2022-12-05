907ba781596ff62bcb97f2713b31680cfca5f178
Ismail Aidar <ismailaidar@gmail.com>
2022-12-05 11:14:22 -0500

    validation is already covered by belongs_to :person

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,7 +5,6 @@ class Doctor < ApplicationRecord
   enum :status, {
     active: 'active', inactive: 'inactive'
   }, default: 'active', prefix: true
-  validates :person, presence: true
   validates :npi, presence: true, length: { is: 10 }, format: { with: /\A[0-9]+\z/, message: 'only allows numbers' },
                   uniqueness: true
 end
