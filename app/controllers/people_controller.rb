61da0384452e715632dbf9a418ddec913ba4e722
Ismail Aidar <ismailaidar@gmail.com>
2022-12-05 18:11:15 -0500

    Should have the redirect like in doctors not found person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -52,6 +52,8 @@ class PeopleController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_person
     @person = Person.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to doctors_url, alert: 'Person not found'
   end
 
   # Only allow a list of trusted parameters through.
