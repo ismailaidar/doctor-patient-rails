1891176b173be285649fb6a1ce4c92bb15e36403
Ismail Aidar <ismailaidar@gmail.com>
2022-12-07 12:04:28 -0500

    change query unassigned_people on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def index
     @last_doctors = Doctor.includes(:person).last(5)
-    @unassigned_people = Person.joins('LEFT OUTER JOIN doctors ON doctors.person_id = people.id').where('doctors.person_id IS null').last(5)
+    @unassigned_people = Person.joins('LEFT OUTER JOIN doctors
+      ON doctors.person_id = people.id
+      LEFT OUTER JOIN patients
+      ON patients.person_id = people.id
+      ').where('doctors.person_id IS null AND patients.person_id IS null').last(5)
   end
 end
