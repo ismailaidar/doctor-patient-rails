f603ac317113ba9dd625fdbe1137dd72abba76f3
Ismail Aidar <ismailaidar@gmail.com>
2022-12-09 09:20:50 -0500

    remove validation check_if_active / use attr instead of obj person

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,20 +6,12 @@ class Patient < ApplicationRecord
                   format: { with: /\A[a-z0-9]+\z/, message: 'only allows Alphanumeric' },
                   uniqueness: true
   validate :check_if_dr_and_pr_are_diff
-  validate :check_if_active_doctor
 
   private
 
   def check_if_dr_and_pr_are_diff
-    return unless doctor
+    return unless doctor_id
 
-    errors.add(:base, 'doctor and patient are the same person') if person == doctor.person
-  end
-
-  def check_if_active_doctor
-    return unless doctor
-
-    # need to be added to PG
-    errors.add(:base, 'this doctor is inactive.') if !doctor.status_active? && doctor_changed?
+    errors.add(:base, 'doctor and patient are the same person') if person_id == doctor_id
   end
 end
